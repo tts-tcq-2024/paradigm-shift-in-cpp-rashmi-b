8d4de72831fe5898090eb6ddc52972b812a74d12
Ballamajalu Rashmi <rashmi.rushi@gmail.com>
2024-07-22 14:09:40 +0530

    updated main and test cases

---
+++ checker_main.cpp
@@ -0,0 +1,15 @@
+#include "checker.hpp"
+
+int main()
+{
+  BatteryManagementSystem BMS;
+  assert(BMS.batteryIsOk(25, 70, 0.7) == true);
+  assert(BMS.batteryIsOk(50, 85, 0) == false);
+  assert(BMS.batteryIsOk(30, 70, 0.1) == true);
+  assert(BMS.batteryIsOk(30, 70, 0.9) == false);
+  assert(BMS.batteryIsOk(1, 70, 0.7) == true);
+  assert(BMS.batteryIsOk(25, 78, 0.7) == true);
+  assert(BMS.batteryIsOk(25, 70, 0.7) == true);
+  assert(BMS.batteryIsOk(25, 70, 0.79) == true);
+  return 0;
+}
