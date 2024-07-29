197397a3622091b8241a35d09e24659ebe35b7b3
Venugopal K <venu.shastri@gmail.com>
2024-07-29 12:41:55 +0530

    Update printBatteryMessageParams.hpp

--- printBatteryMessageParams.hpp
+++ printBatteryMessageParams.hpp
@@ -14,8 +14,8 @@ namespace BMS
 	// Vector to store the console message. Retrieve them using the above enum as index values.
 	// E.g console_messages[1][BMS::ENGLISH] will get "WARNING: Parameter is in border range."
 	static std::vector<std::vector<std::string>> console_messages = { { "OK.", "OK."},
-														{ "WARNING: Parameter is in border range.", "WARNUNG: Parameter liegt im Grenzbereich."},
-														{ "ERROR: Not in Range! Please check.", "FEHLER: Nicht in Reichweite! Überprüfen Sie bitte."}
+						                        { "WARNING: Parameter is in border range.", "WARNUNG: Parameter liegt im Grenzbereich."},
+									{ "ERROR: Not in Range! Please check.", "FEHLER: Nicht in Reichweite! Überprüfen Sie bitte."}
 													  };
 }
 #endif // #define PRINT_BATTERY_MESSAGE_PARAMS_HPP
