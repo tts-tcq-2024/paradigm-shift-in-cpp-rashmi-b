6cbb541047efae5946c82a021718edaa4fee3ca1
Venugopal K <venu.shastri@gmail.com>
2024-08-01 13:16:05 +0530

    Update batteryCheckerMain.cpp

--- batteryCheckerMain.cpp
+++ batteryCheckerMain.cpp
@@ -9,18 +9,19 @@ class BatteryStatusLoggerMock:public IBatteryStatusLogger{
 void test_cases(){
 BatteryStatusLoggerMock fakeLogger;
   BMS::BatteryManagementSystem codeUnderTest { &fakeLogger};
+  assert(codeUnderTest.batteryIsOk(25, 70, 0.7) == true);
+  assert(codeUnderTest.batteryIsOk(50, 85, 0) == false);
+  assert(codeUnderTest.batteryIsOk(30, 70, 0.1) == false);
+  assert(codeUnderTest.batteryIsOk(30, 70, 0.9) == false);
+  assert(codeUnderTest.batteryIsOk(1, 70, 0.7) == true);
+  assert(codeUnderTest.batteryIsOk(25, 78, 0.7) == true);
+  assert(codeUnderTest.batteryIsOk(15, 50, 0.79) == true);
 }
 
 int main()
 {
   BatteryStatusConsoleLogger consoleLogger;
   BMS::BatteryManagementSystem BMS { &consoleLogger};
-  assert(BMS.batteryIsOk(25, 70, 0.7) == true);
-  assert(BMS.batteryIsOk(50, 85, 0) == false);
-  assert(BMS.batteryIsOk(30, 70, 0.1) == false);
-  assert(BMS.batteryIsOk(30, 70, 0.9) == false);
-  assert(BMS.batteryIsOk(1, 70, 0.7) == true);
-  assert(BMS.batteryIsOk(25, 78, 0.7) == true);
-  assert(BMS.batteryIsOk(15, 50, 0.79) == true);
+  
   return 0;
 }
