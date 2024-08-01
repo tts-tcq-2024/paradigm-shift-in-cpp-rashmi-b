ce927607f67969a8537a68417df0e1c6d7acc093
Venugopal K <venu.shastri@gmail.com>
2024-08-01 13:05:49 +0530

    Update IBatteryStatusLogger.hpp

--- IBatteryStatusLogger.hpp
+++ IBatteryStatusLogger.hpp
@@ -1,4 +1,8 @@
+//interface in c++
+#include "printBatteryMessageParams.hpp"
 class IBatteryStatusLogger{
 public:
-
+            virtual void printOkMessage(const std::string& valueType, LANGUAGE_OPTION language_option)=0;
+            virtual void printErrorMessage(const std::string& valueType, LANGUAGE_OPTION language_option)=0;
+            virtual void printWarnMessage(const std::string& valueType, LANGUAGE_OPTION language_option)=0;
 };
