fd7c1153f86c1e0c651a431e65c5995febd31bc3
Venugopal K <venu.shastri@gmail.com>
2024-08-01 13:14:39 +0530

    Update batteryCheckerMain.cpp

--- batteryCheckerMain.cpp
+++ batteryCheckerMain.cpp
@@ -1,8 +1,20 @@
 #include "batteryChecker.hpp"
+class BatteryStatusLoggerMock:public IBatteryStatusLogger{
+
+  public:
+      //void printOkMe....
+      //void ...
+};
+
+void test_cases(){
+BatteryStatusLoggerMock fakeLogger;
+  BMS::BatteryManagementSystem codeUnderTest { &fakeLogger};
+}
 
 int main()
 {
-  BMS::BatteryManagementSystem BMS;
+  BatteryStatusConsoleLogger consoleLogger;
+  BMS::BatteryManagementSystem BMS { &consoleLogger};
   assert(BMS.batteryIsOk(25, 70, 0.7) == true);
   assert(BMS.batteryIsOk(50, 85, 0) == false);
   assert(BMS.batteryIsOk(30, 70, 0.1) == false);
